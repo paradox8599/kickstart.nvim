b365349975843ed3e0bc49db9862e2f074af0a11
paradox8599 <paradox8599@outlook.com>
2025-03-30 04:35:28 +1100

    smart-split

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,13 +7,7 @@
 -- clear search highlights
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<cr>', { desc = 'Clear search highlights' })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('t', '<C-i>', [[<cmd>hide<cr>]], { desc = 'Hide Terminal' })
 vim.keymap.set('t', '<C-h>', [[<cmd>wincmd h<cr>]], { desc = 'Move focus to the left window' })
 vim.keymap.set('t', '<C-j>', [[<cmd>wincmd j<cr>]], { desc = 'Move focus to the lower window' })
 vim.keymap.set('t', '<C-k>', [[<cmd>wincmd k<cr>]], { desc = 'Move focus to the upper window' })
---
+++ lua/plugins/ui/smart-split.lua
@@ -0,0 +1,128 @@
+return {
+  'mrjones2014/smart-splits.nvim',
+
+  event = 'VeryLazy',
+
+  keys = {
+    {
+      '<A-h>',
+      function()
+        require('smart-splits').resize_left()
+      end,
+      desc = 'Resize left',
+    },
+    {
+      '<A-j>',
+      function()
+        require('smart-splits').resize_down()
+      end,
+      desc = 'Resize down',
+    },
+    {
+      '<A-k>',
+      function()
+        require('smart-splits').resize_up()
+      end,
+      desc = 'Resize up',
+    },
+    {
+      '<A-l>',
+      function()
+        require('smart-splits').resize_right()
+      end,
+      desc = 'Resize right',
+    },
+    {
+      '<leader><leader>h',
+      function()
+        require('smart-splits').swap_buf_left()
+      end,
+      desc = 'Swap buffer left',
+    },
+    {
+      '<leader><leader>j',
+      function()
+        require('smart-splits').swap_buf_down()
+      end,
+      desc = 'Swap buffer down',
+    },
+    {
+      '<leader><leader>k',
+      function()
+        require('smart-splits').swap_buf_up()
+      end,
+      desc = 'Swap buffer up',
+    },
+    {
+      '<leader><leader>l',
+      function()
+        require('smart-splits').swap_buf_right()
+      end,
+      desc = 'Swap buffer right',
+    },
+    {
+      '<C-H>',
+      function()
+        require('smart-splits').move_cursor_left()
+      end,
+      desc = 'Move to left split',
+      mode = 'n',
+    },
+    {
+      '<C-J>',
+      function()
+        require('smart-splits').move_cursor_down()
+      end,
+      desc = 'Move to below split',
+      mode = 'n',
+    },
+    {
+      '<C-K>',
+      function()
+        require('smart-splits').move_cursor_up()
+      end,
+      desc = 'Move to above split',
+      mode = 'n',
+    },
+    {
+      '<C-L>',
+      function()
+        require('smart-splits').move_cursor_right()
+      end,
+      desc = 'Move to right split',
+      mode = 'n',
+    },
+    {
+      '<C-Up>',
+      function()
+        require('smart-splits').resize_up()
+      end,
+      desc = 'Resize split up',
+      mode = 'n',
+    },
+    {
+      '<C-Down>',
+      function()
+        require('smart-splits').resize_down()
+      end,
+      desc = 'Resize split down',
+      mode = 'n',
+    },
+    {
+      '<C-Left>',
+      function()
+        require('smart-splits').resize_left()
+      end,
+      desc = 'Resize split left',
+      mode = 'n',
+    },
+    {
+      '<C-Right>',
+      function()
+        require('smart-splits').resize_right()
+      end,
+      desc = 'Resize split right',
+      mode = 'n',
+    },
+  },
+}
