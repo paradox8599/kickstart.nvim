b0790a5dd1cfe28f4d6a701c7a1ef413b171399f
paradox8599 <paradox8599@outlook.com>
2025-03-30 17:39:22 +1100

    arrow.nvim

---
+++ lua/plugins/utils/arrow.lua
@@ -0,0 +1,14 @@
+-- bookmark files and lines
+
+return {
+  'otavioschwanck/arrow.nvim',
+  dependencies = {
+    -- "nvim-tree/nvim-web-devicons",
+    'echasnovski/mini.icons',
+  },
+  opts = {
+    show_icons = true,
+    leader_key = ';', -- Recommended to be a single key
+    buffer_leader_key = ',', -- Per Buffer Mappings
+  },
+}
