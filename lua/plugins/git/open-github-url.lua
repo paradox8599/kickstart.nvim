bf062327d5fc30b2f51e025095e7e555e255c8e1
paradox8599 <paradox8599@outlook.com>
2025-03-30 17:17:26 +1100

    git-conflict & gh url

---
+++ lua/plugins/git/open-github-url.lua
@@ -0,0 +1,11 @@
+return {
+  'tetzng/open-github-url.nvim',
+
+  lazy = true,
+
+  opts = {},
+
+  keys = {
+    { '<leader>gh', '<cmd>OpenGitHubUrlUnderCursor<cr>', desc = 'Open github url' },
+  },
+}
