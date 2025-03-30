652ace51c28347fa23a2540f6a5d68b5e4f8ef59
paradox8599 <paradox8599@outlook.com>
2025-03-30 16:08:28 +1100

    added hex.nvim

---
+++ lua/plugins/editing/hex.lua
@@ -0,0 +1,8 @@
+return {
+  'RaafatTurki/hex.nvim',
+  cmd = { 'HexDump', 'HexToggle', 'HexAssemble' },
+  keys = {
+    { '<leader>ub', '<cmd>HexToggle<cr>', desc = 'Toggle Hex Editor' },
+  },
+  opts = {},
+}
