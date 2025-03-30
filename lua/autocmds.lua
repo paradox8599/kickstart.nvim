f2fa47fdb2a0b2902c3adbff9e2ba3b90eb25e4d
paradox8599 <paradox8599@outlook.com>
2025-03-30 21:41:36 +1100

    yank hightlight color config

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -8,6 +8,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    -- vim.api.nvim_set_hl(0, 'IncSearch', { fg = '#212121', bg = '#FFFFFF' })
+    vim.highlight.on_yank {}
   end,
 })
